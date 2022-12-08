b96fb53354ac93373c2a32c07b5f4aaef5ca1896
naka <nakasyo2792@gmail.com>
2022-12-08 15:10:37 +0900

    update CRUD softdelete #4

--- src/resources/views/admin/expired-owners.blade.php
+++ src/resources/views/admin/expired-owners.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            オーナー一覧
+            期限切れオーナー
         </h2>
     </x-slot>
 
@@ -12,32 +12,28 @@
                     <section class="text-gray-600 body-font">
                         <div class="container px-5 mx-auto">
                             <x-flash-message status="session('status')"></x-flash-message>
-                            <div class="flex justify-end mb-4">
-                                <button onclick="location.href='{{ route('admin.owners.create'); }}'" class="bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">新規登録</button>
-                            </div>
                             <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                                 <table class="table-auto w-full text-left whitespace-no-wrap">
                                     <thead>
                                         <tr>
                                             <th class="px-4 py-4 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
                                             <th class="px-4 py-4 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メール</th>
-                                            <th class="px-4 py-4 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
-                                            <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                                            <th class="px-4 py-4 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">期限が切れた日</th>
                                             <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @foreach ($owners as $owner)
+                                        @foreach ($expiredOwners as $owner)
                                         <tr>
                                             <td class="px-4 py-4">{{ $owner->name }}</td>
                                             <td class=" px-4 py-4">{{ $owner->email }}</td>
                                             <td class="px-4 py-4">{{ $owner->created_at->diffForHumans() }}</td>
-                                            <td class="w-10 text-center">
-                                                <button type="submit" onclick=" location.href='{{ route('admin.owners.edit', [ 'owner' => $owner->id ]); }}'" class="bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">編集</button>
-                                            </td>
-                                            <form id="delete_{{$owner->id}}" method="post" action="{{ route('admin.owners.destroy', ['owner' => $owner->id])}}">
-                                                @csrf @method('delete')
-                                                <a href=“#” data-id="{{ $owner->id }}" onclick="deletePost(this)">削除</a>
+                                            <form id="delete_{{$owner->id}}" method="post" action="{{ route('admin.expired-owners.destroy', ['owner' => $owner->id])}}">
+                                                @csrf
+                                                <td class="px-4 py-3 text-center">
+                                                    <a href="#" data-id="{{ $owner->id }}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 p-2 focus:outline-none hover:bg-red-500 rounded">完全に削除</a>
+                                                </td>
+                                            </form>
                                         </tr>
                                         @endforeach
                                     </tbody>
