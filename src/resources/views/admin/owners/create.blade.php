686ebd435bd91e4c7251319e4cfe00170ce232c5
naka <nakasyo2792@gmail.com>
2022-12-16 04:16:22 +0900

    add shop画像アップロード　#5

--- src/resources/views/admin/owners/create.blade.php
+++ src/resources/views/admin/owners/create.blade.php
@@ -43,7 +43,7 @@
                                             </div>
                                         </div>
                                         <div class="p-2 w-full flex justify-around mt-4">
-                                            <button type="button onclick=" location.href='{{ route('admin.owners.index') }}'" class=" bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
+                                            <button type="button" onclick=" location.href='{{ route('admin.owners.index') }}'" class=" bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
                                             <button type="submit" class="bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">登録する</button>
                                         </div>
                                     </div>
