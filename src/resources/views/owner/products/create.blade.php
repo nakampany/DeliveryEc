6a3f7e8387723485d0db96e8f41f85d54490c540
naka <nakasyo2792@gmail.com>
2022-12-22 11:50:01 +0900

    add productindex #5

--- src/resources/views/owner/products/create.blade.php
+++ src/resources/views/owner/products/create.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form method="post" action="{{ route('owner.products.store')}}">
+                    <form method="post" action="{{ route('owner.products.store') }}">
                         @csrf
                         <div class="-m-2">
                             <div class="p-2 w-1/2 mx-auto">
@@ -48,7 +48,7 @@
                                     <label for="shop_id" class="leading-7 text-sm text-gray-600">販売する店舗</label>
                                     <select name="shop_id" id="shop_id" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                         @foreach ($shops as $shop )
-                                        <option value="{{ $shop->id}}">
+                                        <option value="{{ $shop->id }}">
                                             {{ $shop->name }}
                                         </option>
                                         @endforeach
@@ -62,7 +62,7 @@
                                         @foreach($categories as $category)
                                         <optgroup label="{{ $category->name }}">
                                             @foreach($category->secondary as $secondary)
-                                            <option value="{{ $secondary->id}}">
+                                            <option value="{{ $secondary->id }}">
                                                 {{ $secondary->name }}
                                             </option>
                                             @endforeach
@@ -72,10 +72,10 @@
                                 </div>
                             </div>
                             <x-select-image :images="$images" name="image1" />
-                            <x-select-image :images="$images" name="image2" />
+                            <!-- <x-select-image :images="$images" name="image2" />
                             <x-select-image :images="$images" name="image3" />
                             <x-select-image :images="$images" name="image4" />
-                            <x-select-image :images="$images" name="image5" />
+                            <x-select-image :images="$images" name="image5" /> -->
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative flex justify-around">
                                     <div><input type="radio" name="is_selling" value="1" class="mr-2" checked>販売中</div>
@@ -84,7 +84,7 @@
                             </div>
                             <div class="p-2 w-full flex justify-around mt-4">
                                 <button type="button" onclick="location.href='{{ route('owner.products.index')}}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
-                                <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
+                                <button type="submit" bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
                             </div>
                         </div>
                     </form>
