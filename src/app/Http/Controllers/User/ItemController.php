d1c9de7dc4982719db9accd3e7c60da549a2c9cf
naka <nakasyo2792@gmail.com>
2022-12-27 15:38:37 +0900

    add 販売停止中の商品は404を返す #8

--- src/app/Http/Controllers/User/ItemController.php
+++ src/app/Http/Controllers/User/ItemController.php
@@ -14,49 +14,22 @@ class ItemController extends Controller
     {
         $this->middleware('auth:users');
 
-        // $this->middleware(function ($request, $next) {
-        //     $id = $request->route()->parameter('item');
-        //     if (!is_null($id)) {
-        //         $itemId = Product::availableItems()->where('product.id', $id)->exists();
-        //         if (!$itemId) {
-        //             abort(404);
-        //         }
-        //     }
-        //     return $next($request);
-        // });
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('item');
+            if (!is_null($id)) {
+                $itemId = Product::availableItems()->where('products.id', $id)->exists();
+                if (!$itemId) {
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
     }
 
     public function index(Request $request)
     {
-        // SELECT `product_id`, sum(`quantity`) as `quantity` FROM `t_stocks`
-        // GROUP BY `product_id`
-        // HAVING `quantity` > 1
-        $stocks = DB::table('t_stocks')
-            ->select('product_id', DB::raw('sum(quantity) as quantity'))
-            ->groupBy('product_id')
-            ->having('quantity', '>', 1);
 
-        $products = DB::table('products')
-            ->joinSub($stocks, 'stock', function ($join) {
-                $join->on('products.id', '=', 'stock.product_id');
-            })
-            ->join('shops', 'products.shop_id', '=', 'shops.id')
-            ->join('secondary_categories', 'products.secondary_category_id', '=', 'secondary_categories.id')
-            ->join('images as image1', 'products.image1', '=', 'image1.id')
-            ->join('images as image2', 'products.image2', '=', 'image2.id')
-            ->join('images as image3', 'products.image3', '=', 'image3.id')
-            ->where('shops.is_selling', true)
-            ->where('products.is_selling', true)
-            ->select(
-                'products.id as id',
-                'products.name as name',
-                'products.price',
-                'products.sort_order as sort_order',
-                'products.information',
-                'secondary_categories.name as category',
-                'image1.filename as filename'
-            )
-            ->get();
+        $products = Product::availableItems()->get();
 
         return view('user.index', compact('products'));
     }
