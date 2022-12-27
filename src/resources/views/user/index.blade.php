78c72392a663839b324aa5fd470e50116a45db23
naka <nakasyo2792@gmail.com>
2022-12-27 16:55:08 +0900

    add カテゴリー検索 #8

--- src/resources/views/user/index.blade.php
+++ src/resources/views/user/index.blade.php
@@ -4,9 +4,9 @@
             商品一覧
         </h2>
         <form method="get" action="{{ route('user.items.index')}}">
-            <div class="lg:flex lg:justify-around">
+            <div class="">
                 <div class="flex">
-                    <div>
+                    <div style="margin: 10px;">
                         <span class="text-sm">表示順</span><br>
                         <select id="sort" name="sort" class="mr-4">
                             <option value="{{ \Constant::SORT_ORDER['recommend']}}" @if(\Request::get('sort')===\Constant::SORT_ORDER['recommend'] ) selected @endif>おすすめ順
@@ -17,9 +17,26 @@
                             </option>
                             <option value="{{ \Constant::SORT_ORDER['later']}}" @if(\Request::get('sort')===\Constant::SORT_ORDER['later'] ) selected @endif>新しい順
                             </option>
-                            <option value="{{ \Constant::SORT_ORDER['older']}}" @if(\Request::get('sort')===\Constant::SORT_ORDER['older'] ) selected @endif>古い順
-                            </option>
                         </select>
+                    </div>
+                    <div class="flex justify-center" style="margin: 10px;">
+                        <div>
+                            <span class="text-sm">店舗カテゴリー検索</span><br>
+                            <select name="category" class="mb-2 lg:mb-0 lg:mr-2">
+                                <option value="0" @if(\Request::get('category')==='0' ) selected @endif>全て</option>
+                                @foreach($categories as $category)
+                                <optgroup label="{{ $category->name }}">
+                                    @foreach($category->secondary as $secondary)
+                                    <option value="{{ $secondary->id}}" @if(\Request::get('category')==$secondary->id) selected @endif >
+                                        {{ $secondary->name }}
+                                    </option>
+                                    @endforeach
+                                    @endforeach
+                            </select>
+                        </div>
+                        <div class="flex space-x-2 items-start" style="align-items: flex-end; margin-left: 5px;">
+                            <div><button class="ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">検索する</button></div>
+                        </div>
                     </div>
                 </div>
             </div>
