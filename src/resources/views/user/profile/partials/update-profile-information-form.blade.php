30add9d1fd7a5fbd44f0f9b11c5dff99fd8faaed
nakaji <103278404+nakampany@users.noreply.github.com>
2023-01-06 08:29:34 +0900

    Revert "add バグ修正 ＃12"

--- src/resources/views/user/profile/partials/update-profile-information-form.blade.php
+++ src/resources/views/user/profile/partials/update-profile-information-form.blade.php
@@ -1,14 +1,11 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('ユーザー情報編集') }}
+            {{ __('ユーザーネーム・メール情報編集') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __("アカウント情報を更新できます。") }}
-        </p>
-        <p class="mt-1 text-sm text-red-600">
-            {{ __("※注文の際は、郵便番号・住所・配達住所詳細・電話番号が必須です") }}
+            {{ __("アカウントのプロフィール情報、メールアドレスを更新できます。") }}
         </p>
     </header>
 
@@ -51,25 +48,25 @@
         </div>
 
         <div>
-            <x-input-label for="zip_code" :value="__('郵便番号※必須')" />
+            <x-input-label for="zip_code" :value="__('郵便番号')" />
             <x-text-input id="zip_code" name="zip_code" type="text" class="mt-1 block w-full" :value="old('zip_code', $user->zip_code)" required autofocus autocomplete="zip_code" />
             <x-input-error class="mt-2" :messages="$errors->get('zip_code')" />
         </div>
 
         <div>
-            <x-input-label for="address" :value="__('住所※必須')" />
+            <x-input-label for="address" :value="__('住所')" />
             <x-text-input id="address" name="address" type="text" class="mt-1 block w-full" :value="old('address', $user->address)" required autofocus autocomplete="address" />
             <x-input-error class="mt-2" :messages="$errors->get('address')" />
         </div>
 
         <div>
-            <x-input-label for="address_detail" :value="__('配達住所詳細※必須')" />
+            <x-input-label for="address_detail" :value="__('配達住所詳細')" />
             <x-text-input id="address_detail" name="address_detail" type="text" class="mt-1 block w-full" :value="old('address_detail', $user->address_detail)" required autofocus autocomplete="address_detail" />
             <x-input-error class="mt-2" :messages="$errors->get('address_detail')" />
         </div>
 
         <div>
-            <x-input-label for="tel" :value="__('電話番号※必須')" />
+            <x-input-label for="tel" :value="__('電話番号')" />
             <x-text-input id="tel" name="tel" type="text" class="mt-1 block w-full" :value="old('tel', $user->tel)" required autofocus autocomplete="tel" />
             <x-input-error class="mt-2" :messages="$errors->get('tel')" />
         </div>
